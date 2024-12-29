a7d7449aa7d816058e8d50bfd0f0616e246fd0eb
JustinLung <justinlung77@gmail.com>
2024-12-29 19:50:54 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '$lib/styles/all.css';
+	import Header from '$lib/components/layout/Header.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
+	import Lenis from '$lib/components/shared/Lenis.svelte';
+
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+	import { Flip } from 'gsap/dist/Flip';
+
+	gsap.registerPlugin(ScrollTrigger, Flip);
+</script>
+
+<Lenis />
+<Header />
+<main class="container">
+	<slot />
+</main>
+<Footer />
